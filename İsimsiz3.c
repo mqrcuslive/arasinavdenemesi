8d5fcf145b62122960773322a5fd1abd0039df6a
mqrcuslive <66071178+mqrcuslive@users.noreply.github.com>
2020-05-28 18:03:53 +0300

    Create İsimsiz3.c

---
+++ İsimsiz3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main()
+{ 
+   int karsay;
+   int sayi=1234;
+   karsay =printf("ugurlu sayim=%d\n",sayi);
+   printf("ust satirda karakter sayisi:%ddir\n",karsay);
+   return 0;
+}
